2089fdae5df43dead76d3c054cd0a61acdcf4922
Gabriel <gabrielmora2001@hotmail.com>
2022-03-23 08:06:12 -0500

    Sprint 2

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const myForm = document.getElementById("myForm");
 const csvFile = document.getElementById("csvFile");
 const table = document.getElementById("results");
 const tableHead = document.getElementById("headings");
+const sel1 = document.getElementById("Name1");
+const sel2 = document.getElementById("Name2");
+const compareBtn = document.getElementById("compare");
+const resultTxt = document.getElementById("result");
 let isAsc = false;
 let datos;
 
@@ -53,52 +57,80 @@ function csvToArray(str, delimiter = ",") {
     }
   }
 
-  function sortResults(attr,asc){
-    
-    switch (attr) {
-      case "DMI":
-       if(isAsc){
-        datos.sort(function(a, b){
-          return a.DMI - b.DMI;
-        });
-        table.innerHTML="";
-        generateTable(table,datos);
-       }else if(!isAsc){
-        datos.sort(function(a, b){
-          return b.DMI - a.DMI;
-        });
-        table.innerHTML="";
-        generateTable(table,datos);
-        
-       }
-       console.log(datos);
-        break;
-        case "Hamburguesa":
-          if(isAsc){
-           datos.sort(function(a, b){
-             return a.Hamburguesa - b.Hamburguesa;
-           });
-           table.innerHTML="";
-           generateTable(table,datos);
-          }else if(!isAsc){
-           datos.sort(function(a, b){
-             return b.Hamburguesa - a.Hamburguesa;
-           });
-           table.innerHTML="";
-           generateTable(table,datos);
-          }
-           break;
-      default:
-        break;
-    }
+  function sortResults(attr){
+    if(isAsc){
+      datos.sort(function(a, b){
+        return a[attr] - b[attr];
+      });
+      table.innerHTML="";
+      generateTable(table,datos);
+     }else if(!isAsc){
+      datos.sort(function(a, b){
+          return b[attr] - a[attr];
+      });
+      table.innerHTML="";
+      generateTable(table,datos);
+      
+     }
+     console.log(datos);
   }
 
 tableHead.addEventListener("click", function (f) {
 let attr = f.target.id;
+console.log(attr);
 isAsc = !isAsc;
-sortResults(attr,isAsc);
+sortResults(attr);
 });
 
+compareBtn.addEventListener("click", function(){
+let name1 = sel1.options[sel1.selectedIndex].text;
+let name2 = sel2.options[sel2.selectedIndex].text;
+let Obj1 =[];
+let Obj2 = [];
+for(var i = 0; i < datos.length; i++) {
+  if(name1===datos[i].Nombres){
+  Obj1 = datos[i];
+  }
+}
+for(var i = 0; i < datos.length; i++) {
+  if(name2===datos[i].Nombres){
+   Obj2 = datos[i];
+  }
+}
+let Arr1 = Object.values(Obj1);
+let Arr2 = Object.values(Obj2);
+let Arr1s = Arr1.splice(1);
+let Arr2s = Arr2.splice(1);
+let result = cosineSimil(Arr1s,Arr2s);
+if(result>=0.99){
+resultTxt.innerHTML = "La similitud coseno es: 1<br>¡Increíble, estas dos personas son como almas gemelas!";
+}else{
+  resultTxt.innerHTML = `La similitud coseno es: ${result}`;
+};
+});
+function cosineSimil(Arr1,Arr2){
+  let d = dotProduct(Arr1,Arr2);
+  let m1 = magnitude(Arr1);
+  let m2 = magnitude(Arr2);
+  let result = d/(m1*m2);
+  return result;
+   
+}
+function dotProduct(a,b){
+  var sum = 0;
+for(var i=0; i< a.length; i++) {
+    sum += a[i]*b[i];
+}
+return sum;
+}
+function magnitude(a){
+var sum = 0;
+for(var i=0; i< a.length; i++) {
+  sum += Math.pow(a[i],2);
+}
+let result = Math.sqrt(sum);
+return result;
+}
 myForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const input = csvFile.files[0];
@@ -109,17 +141,24 @@ myForm.addEventListener("submit", function (e) {
     const data = csvToArray(text);
     datos = data;
     generateTable(table,data);
-    
+    fillSelects(data);
 
-   // document.write(JSON.stringify(data));
   };
+ function fillSelects(datos){
+for(var i = 0; i < datos.length; i++) {
+  var opt = document.createElement('option');
+  opt.innerHTML = datos[i].Nombres;
+  opt.value  = datos[i].Nombres;
+  sel1.appendChild(opt);
+}
+for(var i = 0; i < datos.length; i++) {
+  var opt = document.createElement('option');
+  opt.innerHTML = datos[i].Nombres;
+  opt.value  = datos[i].Nombres;
+  sel2.appendChild(opt);
+}
+  }
 
   reader.readAsText(input);
+  
 });
-
-//SORT
-/*
-data.sort(function(a, b){
-  return a.DMI - b.DMI;
-});
-*/
